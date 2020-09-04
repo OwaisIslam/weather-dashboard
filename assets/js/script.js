89fae9cc637684c8dbb21b73abf554aafec40b00
Owais Islam <owaisislam@gmail.com>
2020-09-04 10:05:49 -0500

    created array for future weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 function lookUpCity() {
     // This capitalizes the city name.
     var cityName = ($("#cityName")[0].value.trim().toLowerCase().charAt(0).toUpperCase()) + ($("#cityName")[0].value.trim().toLowerCase().slice(1));
-    var currentDate = moment().format('L');
+
+    // ^^ fix capitalization with two or three word cities
+
+    const currentDate = moment().format('M/D/YYYY');
 
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
 
     fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+
                 $("#city-name")[0].textContent = cityName + " (" + currentDate + ")";
 
-                var latitude = data.coord.lat;
-                var longitude = data.coord.lon;
-                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+                const latitude = data.coord.lat;
+                const longitude = data.coord.lon;
+                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
 
                 fetch(apiURL).then(function (newResponse) {
                     if (newResponse.ok) {
                         newResponse.json().then(function (newData) {
-                            getWeather(newData);
+                            getCurrentWeather(newData);
                         })
                     }
                 })
@@ -28,12 +32,39 @@ function lookUpCity() {
     })
 }
 
-function getWeather(data) {
+function getCurrentWeather(data) {
     console.log(data);
-    $("#temperature")[0].textContent = "Temperature: " + data.current.temp + " \u2109";
+
+    $(".results-panel").addClass("show-element");
+
+    $("#temperature")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
     $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
-    $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
+    $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
     $("#uv-index")[0].textContent = "UV Index: " + data.current.uvi;
+
+    getFutureWeather(data);
+    // convertUnixTime(data);
+}
+
+function getFutureWeather(data) {
+    var futureWeatherList = [];
+    for (var i = 0; i < 5; i++) {
+        var futureWeather = {
+            date: convertUnixTime(data, i),
+            icon: "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + "@2x.png",
+            temp: data.daily[i + 1].temp.day,
+            humidity: data.daily[i + 1].humidity
+        }
+        futureWeatherList.push(futureWeather);
+    }
+
+    console.log(futureWeatherList);
+}
+
+function convertUnixTime(data, index) {
+    const dateObject = new Date(data.daily[index + 1].dt * 1000);
+
+    return (dateObject.toLocaleDateString());
 }
 
 $("#search-button").on("click", function (e) {
